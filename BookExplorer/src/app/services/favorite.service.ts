dbab10c4a8c5d009405816b092519c61d69fb7c8
HeloiseKatharine <katharineheloise@gmail.com>
2024-09-18 22:55:37 -0300

    feat: adicionar arquivos service

---
+++ BookExplorer/src/app/services/favorite.service.ts
@@ -0,0 +1,60 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FavoriteService {
+  private baseUrl = 'http://localhost:5000';
+
+  constructor(private http: HttpClient) {}
+
+  addFavorite(
+    bookId: string,
+    rating: number | null | undefined,
+    notes: number[] | null | undefined,
+    tags: number[] | null | undefined
+  ) {
+    const favoriteData = {
+      bookId: bookId,
+      rating: rating,
+      notes: notes,
+      tags: tags,
+    };
+
+    return this.http.post(`${this.baseUrl}/favorites_list`, favoriteData);
+  }
+
+  getFavoritesList(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/favorites_list`);
+  }
+
+  updateRating(bookId: string, rating: number): Observable<any> {
+    const body = { bookId, rating };
+
+    return this.http.post(`${this.baseUrl}/rating_list`, body);
+  }
+
+  removeFavorite(bookId: string) {
+    return this.http.delete(`${this.baseUrl}/favorites_list/${bookId}`);
+  }
+
+  // Notas
+
+  addNote(bookId: string, noteText: string): Observable<any> {
+    return this.http.post(`${this.baseUrl}/note`, { bookId, noteText });
+  }
+
+  editNote(noteId: string, noteText: string): Observable<any> {
+    return this.http.put(`${this.baseUrl}/note/${noteId}`, { noteText });
+  }
+
+  deleteNote(noteId: string): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/note/${noteId}`);
+  }
+
+  getNotes(bookId: string): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/note/${bookId}`);
+  }
+}
