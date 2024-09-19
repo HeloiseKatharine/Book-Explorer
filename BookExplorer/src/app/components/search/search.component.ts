c73251c321a9057068a99577b4c2ca3d85d34ebe
HeloiseKatharine <katharineheloise@gmail.com>
2024-09-18 23:09:23 -0300

    feat: adiciona algumas funcionalidade no componente search

--- BookExplorer/src/app/components/search/search.component.ts
+++ BookExplorer/src/app/components/search/search.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent {
+  @Input() livro = {
+    id: '1',
+    titulo: 'Aprendendo Angular',
+    autor: 'Aprendendo Angular',
+  };
 
+  searchTerm: string = '';
+  bookSearchTerm: string = '';
+
+  ngOnInit(): void {}
+
+  handleSearch() {
+    console.log(this.searchTerm);
+    this.bookSearchTerm = this.searchTerm;
+  }
 }
