5adfa8252af1afd20c4c969fcf86daffe4ab23b7
HeloiseKatharine <katharineheloise@gmail.com>
2024-09-18 23:13:07 -0300

    feat: update no app

--- BookExplorer/src/app/app-routing.module.ts
+++ BookExplorer/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FavoritesListComponent } from './components/favorites-list/favorites-list.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'lista-de-favoritos',
+    component: FavoritesListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- BookExplorer/src/app/app.module.ts
+++ BookExplorer/src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchComponent } from './components/search/search.component';
+import { FormsModule } from '@angular/forms';
+import { FavoritesListComponent } from './components/favorites-list/favorites-list.component';
+import { HomeComponent } from './components/home/home.component';
+import { BookListComponent } from './components/book-list/book-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { StarRatingComponent } from './components/star-rating/star-rating.component';
+import { FavoriteButtonComponent } from './components/favorite-button/favorite-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SearchComponent
+    SearchComponent,
+    FavoritesListComponent,
+    HomeComponent,
+    BookListComponent,
+    StarRatingComponent,
+    FavoriteButtonComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
